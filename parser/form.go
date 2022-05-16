5e8b8a19dae8ecf2db248b0bc8ae28a27ccb5912
SheryarButt <M.sheryarbutt@gmail.com>
2022-05-16 23:52:49 +0500

    Updated form parsing to identify if the form is a login form

--- parser/form.go
+++ parser/form.go
@@ -1,14 +1,34 @@
 package parser
 
-import "golang.org/x/net/html"
+import (
+	"sync"
+
+	"golang.org/x/net/html"
+)
 
 // hasForm is a global variable that stores whether the web page contains a form.
 var hasForm bool
 
 // parseForm parses the form node and sets the global variable hasForm to true.
-func parseForm(n *html.Node) {
+func parseForm(n *html.Node, wg *sync.WaitGroup) {
 	if n.Type == html.ElementNode && n.Data == "form" {
-		hasForm = true
+		findLogin(n)
+	}
+	wg.Done()
+}
+
+// findLogin finds password field in form to check if the form contains a login form.
+func findLogin(n *html.Node) {
+	if n.Attr != nil {
+		for _, a := range n.Attr {
+			if a.Key == "type" && a.Val == "hidden" { // Using hidden type to identify password field in the form.
+				hasForm = true
+				return
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		findLogin(c)
 	}
 }
 
