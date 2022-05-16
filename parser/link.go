eb808a350be6fe50f2c5b374ebdfd44cb8d79e41
SheryarButt <M.sheryarbutt@gmail.com>
2022-05-16 21:56:06 +0500

    Added methods to count internal/external and accessible/inaccessible links

--- parser/link.go
+++ parser/link.go
@@ -1,33 +1,71 @@
 package parser
 
 import (
+	"net/http"
+	"sync"
+
 	"golang.org/x/net/html"
 )
 
 // Link respresents a link in a web page.
 // Sample: <a href="...."> link text </a>
 type Link struct {
-	Href string
-	Text string
+	Href   string
+	Text   string
+	Type   bool // true if it is an External link, false if it is an Internal link.
+	Status bool // true if the link is valid, false if it is not valid.
 }
+
+var internalLinks uint
+var externalActiveLinks uint
+var externalDeadLinks uint
 
 // links is a global variable that stores all the links found in the web page.
 var links []Link
 
 // parseLink parses the link node and adds it to the links slice.
-func parseLink(n *html.Node) {
+func parseLink(n *html.Node, wg *sync.WaitGroup) {
 	for _, a := range n.Attr {
 		if a.Key == "href" {
 			link := Link{
-				Href: a.Val,
-				Text: getText(n),
+				Href:   a.Val,
+				Text:   getText(n),
+				Type:   getType(a.Val),
+				Status: getStatus(a.Val),
 			}
 			links = append(links, link)
 		}
 	}
+	wg.Done()
+}
+
+// getType returns true if the link is an external link, false if it is an internal link.
+func getType(s string) bool {
+	if s[0] == 'h' {
+		return true
+	}
+	internalLinks++
+	return false
+}
+
+// getStatus returns true if the link is valid, false if it is not valid.
+func getStatus(s string) bool {
+	if s[0] == 'h' {
+		_, err := http.Get(s)
+		if err == nil {
+			externalActiveLinks++
+			return true
+		}
+		externalDeadLinks++
+	}
+	return false
 }
 
 // GetLinks returns the links found in the web page.
 func GetLinks() []Link {
 	return links
+}
+
+func GetLinkCount() (uint, uint, uint) {
+	return internalLinks, externalActiveLinks, externalDeadLinks
 }
