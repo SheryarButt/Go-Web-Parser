089801de2a3605dc1ef4493cddef119c64b3d4a4
SheryarButt <M.sheryarbutt@gmail.com>
2022-05-18 16:42:37 +0500

    Updated main as http server

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"Task_Home24/parser"
+	"Task_Home24/controller"
 	"fmt"
 	"net/http"
 )
@@ -9,25 +9,10 @@ import (
 // main is the entry point of the program.
 func main() {
 
-	url := "https://www.w3schools.com/html/html_forms.asp"
-	fmt.Printf("HTML code of %s ...\n", url)
-	resp, err := http.Get(url)
-
-	// handle the error if there is one
-	if err != nil {
-		panic(err)
+	mux := controller.Register()
+	fmt.Println("Server is running on port 8080")
+	error := http.ListenAndServe(":8080", mux)
+	if error != nil {
+		panic(error)
 	}
-
-	// doing this now so it won't be forgotten
-	defer resp.Body.Close()
-
-	err = parser.Parse(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(parser.GetLinks())
-	fmt.Println(parser.GetHeadings())
-	fmt.Println(parser.GetForm())
-
 }
